c53cf9ac52c9908db157f4de6c6b9bd69f900fdf
babotavo419 <one.faith419@gmail.com>
2023-05-24 17:21:00 -0400

    i missed the part

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@
 import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
-import SearchBar from './SearchBar';
-import Posts from './Posts';
-import dummyData from './dummyData';
-import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import Posts from './components/Posts/Posts';
+import dummyData from './dummy-data';
 import './App.css';
 
 const App = () => {
@@ -19,6 +18,7 @@ const App = () => {
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
   const [posts, setPosts] = useState(dummyData);
+
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -31,12 +31,15 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-        setPosts((prevPosts) =>
-        prevPosts.map((post) =>
-          post.id === postId ? { ...post, likes: post.likes + 1 } : post
-        )
-      );
-    };
+    const updatedPosts = posts.map(post => {
+      if (post.id === postId) {
+        return {...post, likes: post.likes + 1 }
+  } else; {
+        return post;
+  }
+})
+        setPosts(updatedPosts);
+}
 
   return (
     <div className='App'>
